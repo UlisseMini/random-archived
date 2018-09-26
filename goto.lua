85d208f9d9df0bf896f485670cffc792530304bd
ScriptKidy <ulisse.mini@gmail.com>
2018-09-26 10:31:20 -0400

    Updated goto.lua

--- goto.lua
+++ goto.lua
@@ -3,21 +3,40 @@
 
 -- Args --
 local args = { ... }
-if #args ~= 6 then
-  print('Usage: goto <turtleX> <turtleY> <turtleZ> <destX> <destY> <destZ>')
+if #args ~= 7 then
+  print('Usage: goto <turtleX> <turtleY> <turtleZ> <destX> <destY> <destZ> <turtleOrientation>')
   error()
 end
 
 -- Varables --
-
+local debug_level = 4
 -- Turtle cords
 local x = tonumber(args[1])
 local y = tonumber(args[2])
 local z = tonumber(args[3])
+
 -- Turtle orientation 
--- Copied code not using this yet
-local orientation = 3
-local orientations = {}
+local orientation = tonumber(args[7]) -- orientation
+local orientations = {
+  [0] = "north",
+  [1] = "east",
+  [2] = "south",
+  [3] = "west"
+}
+
+-- Makes sure orientation is a string
+if type(orientation) ~= 'string' then
+  print('Orientation must be north east west or south')
+  error()
+end
+
+-- Turns orientation from a string into its number
+for i=0,#orientations do
+  if orientation == orientations[i] then
+    orientation = i
+    break
+  end
+end
 
 -- Destination cords
 local dX = tonumber(args[4])
@@ -25,8 +44,10 @@ local dY = tonumber(args[5])
 local dZ = tonumber(args[6])
 
 -- Logging function
-local function log(msg)
-  print(msg)
+local function log(msg, msg_debug_level)
+  if debug_level < msg_debug_level then
+    print(msg)
+  end
 end
 
 -- Moving up and down
@@ -50,13 +71,43 @@ local function down()
   end
 end
 
--- other move functions (more complex)
+-- Turning functions
+-- Store it in an integer. Turn right? facing = facing + 1 % 4  Turn left? facing = facing +3 % 4
+local function right()
+  turtle.turnRight()
+  -- Orientation
+  orientation = (orientation + 1) % 4
+end
 
--- Test functions
+local function left()
+  turtle.turnLeft()
+  -- Orientation
+  orientation = (orientation - 1) % 4
+end
 
+-- Main moveto function
 local function moveto(x, y, z)
   -- Work in progress function so far only does y (orientation is a bitch)
+  -- Moves to y
   while dY <= y do
+    turtle.digDown()
+    down()
+  end
+
+  while dY >= y do
+    turtle.digUp()
     up()
   end
+
+end
+-- Tests orientation
+
+for i=1,5 do
+  right()
+  log('[DEBUG] i am looking '..orientations[orientation])
+end
+
+for i=1,5 do
+  left()
+  log('[DEBUG] i am looking '..orientations[orientation])
 end
