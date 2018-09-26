8097feda753fc8c50680400c1f01239587a3cdf7
ScriptKidy <ulisse.mini@gmail.com>
2018-09-25 20:48:16 -0400

    Added shiet

---
+++ miner.lua
@@ -0,0 +1,31 @@
+-- Args --
+args = { ... }
+if #args ~= 1 then
+  print('Usage: mine <distance>')
+  error()
+end
+
+-- Varables --
+local distance = tonumber(args[1])
+local torchslot = 15
+local fuelslot = 16
+
+local tunnelDistance
+
+-- Functions
+local function digTunnel(tdist)
+  -- Digs a tunnel.
+  turtle.turnRight()
+  for i=1,tdist do
+    turtle.dig()
+    turtle.forward()
+    turtle.digUp()
+  end
+  -- Places Torch
+  turtle.select(torchslot)
+  if turtle.getItemCount() == 0 then returnHome() end -- return home if out of torches
+  turtle.placeUp()
+  -- Returns to main tunnel.
+  for i=1,tdist do turtle.back() end
+end
+
