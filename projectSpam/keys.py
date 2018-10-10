f6b155629642dfb158d01c8aa723b841ace64a19
ScriptKidy <ulisse.mini@gmail.com>
2018-10-10 15:10:34 -0400

    Added some python spam programs that i bully my friends with

--- projectSpam/keys.py
+++ projectSpam/keys.py
@@ -5,13 +5,16 @@ from time import sleep
 pressEnter = True
 
 text = str(input('Text to repeat\n> '))
+text = text.lower() # keyboard.write does not like capital letters
+
 times = int(input('Times to repeat {}\n> '.format(text)))
 sleeptime = float(input('How long to sleep between repeats\n> '))
+
 print('Starting in 5 seconds...')
 sleep(5)
 
 for i in range(times):
-    keyboard.write(text)
+    keyboard.write(text, delay=0.05)
     # If pressEnter = True then press enter after writing text.
     if pressEnter == True:
         # Slight delay because computers are not perfect.
